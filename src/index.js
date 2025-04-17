93c17f8a4417d7c1952c5fe54dbafb9f102ccb9e
titanedits99 <gamingzoya605@gmail.com>
2025-04-17 10:40:25 +0530

    Create index.js

---
+++ src/index.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import SiphraGPT from './SiphraGPT';
+
+ReactDOM.render(
+  <React.StrictMode>
+    <SiphraGPT />
+  </React.StrictMode>,
+  document.getElementById('root')
+);
