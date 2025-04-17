46e3cda78b3e1ca2a314a5fb1c30e5dd0139ede7
titanedits99 <gamingzoya605@gmail.com>
2025-04-17 10:46:59 +0530

    Create SiphraGPT.js

---
+++ src/SiphraGPT.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+function SiphraGPT() {
+  return (
+    <div>
+      <h1>Namaste, main Siphra hoon! 😊</h1>
+      <p>Aapka din kaisa jaa raha hai?</p>
+    </div>
+  );
+}
+
+export default SiphraGPT;
+Create SiphraGPT.js file
